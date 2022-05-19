556e99cfd313373e965df351ee1e43166d0dd25a
Paulasamirisaiah <75031362+Paulasamirisaiah@users.noreply.github.com>
2022-05-19 21:49:22 +0200

    first commit

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from statistics import mode
 from unicodedata import name
 from django.db import models
-
+from numpy import number
 
 class category(models.Model):
     name = models.CharField(max_length=70)
@@ -17,3 +17,12 @@ class product(models.Model):
     catagory_id = models.ForeignKey(category,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+
+class cart(models.Model):
+    prod = models.DecimalField(max_digits=11,decimal_places=0)
+    no = models.DecimalField(max_digits=11,decimal_places=0)
+
+
+
+
+
--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import category,product
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from numpy import number
+from .models import category,product,cart
 
 def home(request):
     allcategory = category.objects.all()
@@ -13,7 +15,32 @@ def Category(request,categoryid):
 
 def Product(request,productid):
     allcategory = category.objects.all()
-    myproduct = product.objects.get(id=productid)
     allproducts = product.objects.all().filter(id=productid).order_by("-id")    
-    return render(request,'pages/product.html',{"allcategory":allcategory,"myproduct":myproduct,"allproducts":allproducts})
+    return render(request,'pages/product.html',{"allcategory":allcategory,"allproducts":allproducts})
 
+def Cart(request):
+    allcategory = category.objects.all()
+    allproducts = product.objects.all()
+    number =cart.objects.all().count()
+    carts=cart.objects.all()
+    return render(request,'pages/cart.html',{'cart':carts,"allproducts":allproducts,'number':number,"allcategory":allcategory})
+
+def add(request,productid):
+    x=int(cart.objects.filter(prod=productid).count())
+    if x >= 1:
+        s=cart.objects.get(prod=productid)
+        cart.objects.filter(prod=productid).update(no=int(s.no)+1)
+    else:
+        carts=cart(prod=productid,no=1)
+        carts.save()
+    return redirect("/")
+
+def remove(request,proid):
+    x=int(cart.objects.filter(prod=proid).count())
+    if x!=0:
+        q=cart.objects.get(prod=proid)
+        if q.no >1:
+            cart.objects.filter(prod=proid).update(no=int(q.no)-1)
+        else:
+            q.delete()
+    return redirect("/cart/")
